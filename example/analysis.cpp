eabecd35fb0487ed4872814debeda43394c1639c
Karl Yerkes <karl.yerkes@gmail.com>
2019-01-31 07:50:40 -0800

    better

--- example/analysis.cpp
+++ example/analysis.cpp
@@ -10,7 +10,7 @@ using namespace al;
 #include "synths.h"
 using namespace diy;
 
-#include <fstream>
+#include <vector>
 using namespace std;
 
 const int WINDOW_SIZE = 2048;
@@ -18,119 +18,173 @@ const int FFT_SIZE = 1 << 18;
 
 struct Peak {
   float magnitude, frequency;
-  // float frequency, magnitude;
 };
 
 struct Frame {
   vector<Peak> peak;
-  float frameTimeSeconds;
-  float rootMeanSquare;
-  float amplitudeMinimum;
-  float amplitudeMaximum;
-  float zeroCrossingRate;
-  float magnitudeMaximum;
-  float magnitudeSum;
-  float magnitudeMean;
-  float spectralCentroid;
+
+  float timeSeconds = 0;
+  float rootMeanSquare = 0;
+  float amplitudeMinimum = 0;
+  float amplitudeMaximum = 0;
+  float zeroCrossingRate = 0;
+  float magnitudeMaximum = 0;
+  float magnitudeSum = 0;
+  float magnitudeMean = 0;
+  float spectralCentroid = 0;
+
   void print() {
-    cout << frameTimeSeconds << ' ';
-    cout << rootMeanSquare << ' ';
+    cout << timeSeconds << ' ';
     cout << amplitudeMinimum << ' ';
     cout << amplitudeMaximum << ' ';
-    cout << zeroCrossingRate << ' ';
-    cout << magnitudeMaximum << ' ';
+    cout << rootMeanSquare << ' ';
     cout << magnitudeSum << ' ';
+    cout << magnitudeMaximum << ' ';
     cout << magnitudeMean << ' ';
     cout << spectralCentroid << ' ';
-    for (auto& p : peak)
-      cout << p.frequency << ':' << atodb(p.magnitude) << ' ';
+    cout << zeroCrossingRate << ' ';
+    for (auto& p : peak) cout << p.frequency << ':' << p.magnitude << ' ';
     cout << endl;
   }
 };
 
-void load(vector<Frame>& frame, string fileName) {
-  vector<float> data;
-  float sampleRate;
-
-  {
-    SoundFile soundFile;
-    if (!soundFile.openRead(fileName)) {
-      cout << "failed to open " << fileName << endl;
-      exit(-1);
-    }
-    data.resize(soundFile.frames());
-    soundFile.readAll(&data[0]);
-    sampleRate = soundFile.frameRate();
+struct MyApp : App {
+  string fileName;
+  MyApp(int argc, char* argv[]) {
+    fileName = "../sound/6.wav";
+    if (argc > 1) fileName = argv[1];
   }
 
-  STFT stft(
-      WINDOW_SIZE,
-      WINDOW_SIZE / 4,         // hop size
-      FFT_SIZE - WINDOW_SIZE,  // pad size; appended to window
-      HANN,                    // window type:
-      MAG_PHASE  // format of frequency samples: COMPLEX, MAG_PHASE, or MAG_FREQ
-  );
+  vector<Frame> frame;
+  void onCreate() override {
+    vector<float> sample;
+    float sampleRate;
 
-  // It is important to tell Gamma the sample rate otherwise, it assumes a
-  // sample rate of 1 (1 Hz, 1 sample/second)!
-  gam::Sync::master().spu(sampleRate);
-
-  for (int i = 0; i < data.size(); ++i)
-    if (stft(data[i])) {
-      // push a new frame and make an alias, f
-      frame.push_back(Frame());
-      Frame& f(frame.back());
-
-      // calculate time-domain statistics
-      //
-      f.frameTimeSeconds = i / sampleRate;
-      f.amplitudeMinimum = 0;
-      f.amplitudeMaximum = 0;
-      for (int j = 0; j < WINDOW_SIZE; ++j) {
-        if (data[i - j] > f.amplitudeMaximum) f.amplitudeMaximum = data[i - j];
-        if (data[i - j] < f.amplitudeMinimum) f.amplitudeMinimum = data[i - j];
+    {
+      SoundFile soundFile;
+      if (!soundFile.openRead(fileName)) {
+        cout << "failed to open " << fileName << endl;
+        exit(-1);
       }
-      double t = 0;
-      for (int j = 0; j < WINDOW_SIZE; ++j) t += data[i - j] * data[i - j];
-      f.rootMeanSquare = sqrt(t / WINDOW_SIZE);
-      f.zeroCrossingRate = 0;
-      for (int j = 1; j < WINDOW_SIZE; ++j)
-        if (data[i - j - 1] * data[i - j] < 0) f.zeroCrossingRate += 1.0;
-      f.zeroCrossingRate /= (float)WINDOW_SIZE / SAMPLE_RATE;
-
-      // calculate frequency-domain statistics
-      //
-      f.magnitudeSum = 0;
-      for (int b = 0; b < stft.numBins(); ++b) f.magnitudeSum += stft.bin(b)[0];
-      f.magnitudeMaximum = 0;
-      for (int b = 0; b < stft.numBins(); ++b)
-        if (f.magnitudeMaximum < stft.bin(b)[0])
-          f.magnitudeMaximum = stft.bin(b)[0];
-      f.magnitudeMean = f.magnitudeSum / stft.numBins();
-      f.spectralCentroid = 0;
-      for (int b = 0; b < stft.numBins(); ++b)
-        f.spectralCentroid += stft.bin(b)[0] * stft.binFreq() * b;
-      f.spectralCentroid /= f.magnitudeSum;
-
-      // collect every maxima
-      for (int b = 1; b < stft.numBins() - 1; ++b) {
-        float m[3]{stft.bin(b - 1)[0], stft.bin(b)[0], stft.bin(b + 1)[0]};
-        if (m[1] > m[0] && m[1] > m[2])
-          f.peak.push_back({m[1], static_cast<float>(stft.binFreq() * b)});
-      }
-      // sort maxima by magnitude
-      sort(f.peak.begin(), f.peak.end(), [](const Peak& a, const Peak& b) {
-        return a.magnitude < b.magnitude;
-      });
-      // throw away all but N peaks
-      f.peak.resize(100);
+      sample.resize(soundFile.frames());
+      soundFile.readAll(&sample[0]);
+      sampleRate = soundFile.frameRate();
     }
-}
+
+    STFT stft(WINDOW_SIZE,
+              WINDOW_SIZE / 4,         // hop size
+              FFT_SIZE - WINDOW_SIZE,  // pad size; appended to window
+              HANN,                    // window type:
+              MAG_PHASE  // format of frequency samples: COMPLEX, MAG_PHASE, or
+                         // MAG_FREQ
+    );
+
+    // It is important to tell Gamma the sample rate otherwise, it assumes a
+    // sample rate of 1 (1 Hz, 1 sample/second)!
+    gam::Sync::master().spu(sampleRate);
+
+    for (int i = 0; i < sample.size(); ++i)
+      if (stft(sample[i])) {
+        // push a new frame and make an alias, f
+        frame.emplace_back();
+        Frame& f(frame.back());
+        float* s = &sample[i - WINDOW_SIZE];
+
+        f.timeSeconds = i / sampleRate;
+
+        // amplitude extrema
+        //
+        f.amplitudeMinimum = 0;
+        f.amplitudeMaximum = 0;
+        for (int j = 0; j < WINDOW_SIZE; ++j) {
+          if (s[j] > f.amplitudeMaximum) {
+            f.amplitudeMaximum = s[j];
+          }
+          if (s[j] < f.amplitudeMinimum) {
+            f.amplitudeMinimum = s[j];
+          }
+        }
+
+        // RMS
+        double t = 0;
+        for (int j = 0; j < WINDOW_SIZE; ++j) {
+          t += s[j] * s[j];
+        }
+        f.rootMeanSquare = sqrt(t / WINDOW_SIZE);
+
+        // ZCR
+        f.zeroCrossingRate = 0;
+        for (int j = 1; j < WINDOW_SIZE; ++j)
+          if (s[j - 1] * s[j] < 0) {
+            f.zeroCrossingRate += 1.0;
+          }
+        f.zeroCrossingRate /= WINDOW_SIZE / sampleRate;
+
+        // sum of all magnitudes
+        f.magnitudeSum = 0;
+        for (int b = 0; b < stft.numBins(); ++b) {
+          f.magnitudeSum += stft.bin(b)[0];
+        }
+
+        // largest magnitude
+        f.magnitudeMaximum = 0;
+        for (int b = 0; b < stft.numBins(); ++b)
+          if (f.magnitudeMaximum < stft.bin(b)[0])
+            f.magnitudeMaximum = stft.bin(b)[0];
+
+        // average
+        f.magnitudeMean = f.magnitudeSum / stft.numBins();
+
+        // center of gravity of the spectrum
+        f.spectralCentroid = 0;
+        for (int b = 0; b < stft.numBins(); ++b)
+          f.spectralCentroid += stft.bin(b)[0] * stft.binFreq() * b;
+        f.spectralCentroid /= f.magnitudeSum;
+
+        // collect every maxima
+        for (int b = 1; b < stft.numBins() - 1; ++b) {
+          float m[3]{stft.bin(b - 1)[0], stft.bin(b)[0], stft.bin(b + 1)[0]};
+          if (m[1] > m[0] && m[1] > m[2]) {
+            f.peak.emplace_back();
+            f.peak.back().magnitude = m[1];
+            f.peak.back().frequency = stft.binFreq() * b;
+          }
+        }
+
+        // sort maxima by magnitude
+        sort(f.peak.begin(), f.peak.end(), [](const Peak& a, const Peak& b) {
+          return a.magnitude > b.magnitude;
+        });
+        // throw away all but N peaks
+        f.peak.resize(5);
+
+        if (1) {
+          f.magnitudeMaximum = atodb(f.magnitudeMaximum);
+          f.magnitudeMean = atodb(f.magnitudeMean);
+          f.magnitudeSum = atodb(f.magnitudeSum);
+          for (auto& p : f.peak) p.magnitude = atodb(p.magnitude);
+        }
+      }
+    // for (auto& f : frame) f.print();
+    // exit(1);
+  }
+
+  void onDraw(Graphics& g) override {
+    g.clear(0.25);
+    //
+  }
+
+  void onSound(AudioIOData& io) override {
+    while (io()) {
+      float f = 0;
+      io.out(0) = f;
+      io.out(1) = f;
+    }
+  }
+};
 
 int main(int argc, char* argv[]) {
-  string fileName = "../sound/6.wav";
-  if (argc > 1) fileName = argv[1];
-  vector<Frame> frame;
-  load(frame, fileName);
-  for (auto& f : frame) f.print();
+  MyApp app(argc, argv);
+  app.initAudio(SAMPLE_RATE, BLOCK_SIZE, OUTPUT_CHANNELS, INPUT_CHANNELS);
+  app.start();
 }
